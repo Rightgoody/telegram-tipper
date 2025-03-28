984152fb22b9ec231ac872623f7ba124d73b8671
Joshua Park <rightgoody@csu.fullerton.edu>
2025-03-27 18:11:02 -0700

    Pushing code to fork. Will send PR for IZIO to review

---
+++ telegram_bot/src/commands/send.rs
@@ -0,0 +1,173 @@
+use core::{
+    error::Error as SpectreError,
+    tip_context::TipContext,
+    tip_transition_wallet::TipTransitionWallet,
+    utils::{get_tx_explorer_url, try_parse_required_nonzero_spectre_as_sompi_u64},
+};
+use std::sync::Arc;
+
+use spectre_wallet_core::tx::{Fees, PaymentOutputs};
+use spectre_wallet_keys::secret::Secret;
+use teloxide::{Bot, types::Message};
+use user::user::TipUser;
+use workflow_core::prelude::Abortable;
+
+use crate::error::TelegramBotError;
+
+pub async fn command_send(
+    bot: Bot,
+    _msg: &Message,
+    tip_context: Arc<TipContext>,
+    tip_sender: &TipUser,
+    password: String,
+    amount: String,
+    recipient_username: String,
+    tip_message: Option<String>, //optional tip message!
+) -> Result<(), TelegramBotError> {
+    let is_opened = tip_context.does_opened_owned_wallet_exists(tip_sender.wallet_identifier());
+    let is_initiated = match is_opened {
+        true => true,
+        false => {
+            tip_context
+                .local_store()?
+                .exists(Some(tip_sender.wallet_identifier()))
+                .await?
+        }
+    };
+
+    if !is_initiated {
+        tip_sender
+            .send_telegram_message(&bot, "Error, wallet not initiated yet")
+            .await?;
+
+        return Ok(());
+    }
+
+    if !is_opened {
+        tip_sender
+            .send_telegram_message(&bot, "Error, wallet not opened")
+            .await?;
+
+        return Ok(());
+    }
+
+    let tip_wallet = match tip_context.get_opened_owned_wallet(tip_sender.wallet_identifier()) {
+        Some(w) => w,
+        None => {
+            tip_sender
+                .send_telegram_message(&bot, "Unexpected error: wallet not opened")
+                .await?;
+
+            return Ok(());
+        }
+    };
+
+    let amount_sompi = try_parse_required_nonzero_spectre_as_sompi_u64(Some(amount))?;
+    println!("amount sompi {}", amount_sompi);
+
+    let wallet = tip_wallet.wallet();
+
+    // @TODO(izio/tg) since we're unable to confirm the recipient exists, we need to double confirm with end-user (discussion flow)
+    let recipient_identifier = format!("tg-{}", recipient_username.to_lowercase());
+
+    // find address of recipient or create a temporary wallet
+    let existing_owned_wallet = tip_context
+        .owned_wallet_metadata_store
+        .find_owned_wallet_metadata_by_owner_identifier(&recipient_identifier)
+        .await;
+
+    let recipient_address = match existing_owned_wallet {
+        Ok(wallet) => wallet.receive_address,
+        Err(SpectreError::OwnedWalletNotFound()) => {
+            // find or create a temporary wallet
+            let transition_wallet_result = tip_context
+                .transition_wallet_metadata_store
+                .find_transition_wallet_metadata_by_identifier_couple(
+                    tip_sender.wallet_identifier(),
+                    &recipient_identifier,
+                )
+                .await?;
+
+            match transition_wallet_result {
+                Some(wallet) => wallet.receive_address,
+                None => TipTransitionWallet::create(
+                    tip_context.clone(),
+                    tip_sender.wallet_identifier(),
+                    &recipient_identifier,
+                )
+                .await?
+                .receive_address(),
+            }
+        }
+        Err(e) => {
+            tip_sender
+                .send_telegram_message(&bot, format!("Error: {:}", e))
+                .await?;
+            return Ok(());
+        }
+    };
+
+    let address = recipient_address;
+
+    let outputs = PaymentOutputs::from((address, amount_sompi));
+    let abortable = Abortable::default();
+    let wallet_secret = Secret::from(password);
+
+    let account = wallet.account()?;
+
+    let (summary, hashes) = match account
+        .send(
+            outputs.into(),
+            Fees::SenderPays(0),
+            None,
+            wallet_secret,
+            None,
+            &abortable,
+            Some(Arc::new(
+                move |ptx: &spectre_wallet_core::tx::PendingTransaction| {
+                    println!("tx notifier: {:?}", ptx);
+                },
+            )),
+        )
+        .await
+    {
+        Ok(result) => result,
+        Err(e) => {
+            tip_sender
+                .send_telegram_message(&bot, format!("Error, transaction failed: {:}", e))
+                .await?;
+
+            return Ok(());
+        }
+    };
+
+    let tx_id = hashes[0].to_string();
+
+    // public mentionning
+    tip_sender
+        .send_telegram_message(
+            &bot,
+            format!(
+                "Transaction Successful: you sent @{}: {}\n\nTxid: {:?}\nExplorer: {}",
+                recipient_identifier,
+                summary,
+                tx_id,
+                get_tx_explorer_url(&tx_id, tip_context.network_id().network_type())
+            ),
+        )
+        .await?;
+
+    // private message sent to user from bot~
+    if let Some(message) = tip_message {
+        let recipient_chat_id = bot
+            .get_chat_id_from_user(&recipient_username)
+            .await
+            .map_err(|_| TelegramBotError::Custom("Failed to get reciever chat ID".to_string()))?;
+
+        bot.send_message(recipient_chat_id, message).await?;
+    }
+    // @TODO(izio/tg): how to deal with private mentionning? Is this even possible?
+    //  -> https://stackoverflow.com/a/78459144  ~ Is this what you had mentioned to me?
+
+    Ok(())
+}
---
+++ telegram_bot/src/main.rs
@@ -0,0 +1,285 @@
+use core::{tip_context::TipContext, utils::check_node_status};
+use spectre_wallet_core::rpc::ConnectOptions;
+use spectre_wrpc_client::{
+    Resolver, SpectreRpcClient, WrpcEncoding,
+    prelude::{ConnectStrategy, NetworkId},
+};
+use std::{env, path::Path, str::FromStr, sync::Arc, time::Duration};
+use telegram_bot::{
+    commands::{
+        Command, change_password::command_change_password, claim::command_claim,
+        close::command_close, create::command_create, destroy::command_destroy,
+        export::command_export, open::command_open, restore::command_restore, send::command_send,
+        status::command_status, withdraw::command_withdraw,
+    },
+    error::{LoggingErrorHandler, TelegramBotError},
+};
+use tracing::{debug, error, info, warn};
+use tracing_subscriber::EnvFilter;
+use user::user::TipUser;
+
+use teloxide::utils::command::BotCommands;
+use teloxide::{dispatching::dialogue::GetChatId, prelude::*};
+
+#[tokio::main]
+async fn main() {
+    // load local .env or ignore if file doesn't exists
+    match dotenvy::dotenv() {
+        Ok(_) => println!("Environment variables loaded from .env"),
+        Err(_) => println!("Not loading environement variables from .env"),
+    }
+
+    tracing_subscriber::fmt()
+        .with_env_filter(EnvFilter::from_default_env())
+        .init();
+
+    let telegram_token = match env::var("TELEGRAM_TOKEN") {
+        Ok(v) => v,
+        Err(_) => panic!("TELEGRAM_TOKEN environment variable is missing."),
+    };
+
+    let spectre_network_str =
+        env::var("SPECTRE_NETWORK").expect("SPECTRE_NETWORK environment variable is missing");
+
+    let wallet_data_path_str =
+        env::var("WALLET_DATA_PATH").expect("WALLET_DATA_PATH environment variable is missing");
+
+    // RPC
+    let forced_spectre_node: Option<String> = match env::var("FORCE_SPECTRE_NODE_ADDRESS") {
+        Ok(v) => Some(v),
+        Err(_) => None,
+    };
+
+    let resolver = match forced_spectre_node.clone() {
+        Some(value) => Resolver::new(Some(vec![Arc::new(value)]), true), // tls
+        _ => Resolver::default(),
+    };
+
+    let network_id = NetworkId::from_str(&spectre_network_str).unwrap();
+
+    let wrpc_client = Arc::new(
+        SpectreRpcClient::new(
+            WrpcEncoding::Borsh,
+            forced_spectre_node.as_deref(),
+            Some(resolver.clone()),
+            Some(network_id),
+            None,
+        )
+        .unwrap(),
+    );
+
+    let connect_timeout = Duration::from_secs(5);
+
+    match wrpc_client
+        .connect(Some(ConnectOptions {
+            url: forced_spectre_node.clone(),
+            block_async_connect: true,
+            connect_timeout: Some(connect_timeout),
+            strategy: ConnectStrategy::Fallback,
+            ..Default::default()
+        }))
+        .await
+    {
+        Ok(_) => info!(
+            "Node {} is reachable, checking capabilities.",
+            wrpc_client.ctl().descriptor().unwrap()
+        ),
+        Err(e) => {
+            error!("Failed to connect to the node: {}", e);
+            panic!("Connection failed: {}", e);
+        }
+    }
+
+    match check_node_status(&wrpc_client).await {
+        Ok(_) => {
+            info!("Successfully completed client connection to the Spectre node!");
+        }
+        Err(error) => {
+            error!("An error occurred: {}", error);
+            std::process::exit(1);
+        }
+    }
+
+    let wallet_data_path_buf = Path::new(&wallet_data_path_str).to_path_buf();
+
+    let tip_context = TipContext::try_new_arc(
+        resolver,
+        NetworkId::from_str(&spectre_network_str).unwrap(),
+        forced_spectre_node,
+        wrpc_client,
+        wallet_data_path_buf,
+    );
+
+    if let Err(e) = tip_context {
+        panic!("{}", format!("Error while building tip context: {}", e));
+    }
+
+    let bot = Bot::new(telegram_token);
+
+    let commands_handler = dptree::entry()
+        .filter_command::<Command>()
+        .endpoint(command_handler);
+
+    let message_create_handler = Update::filter_message().branch(commands_handler);
+
+    let main_handler = dptree::entry()
+        .branch(message_create_handler)
+        // default handler
+        .branch(
+            dptree::entry().endpoint(|bot: Bot, update: Update| async move {
+                debug!("Received update: {:?}", update);
+                match update.chat_id() {
+                    Some(chat_id) => {
+                        bot.send_message(chat_id, format!("Unknown command or argument format is not correct.\nPlease use /help to see the list of available commands.")).await?;
+                    }
+                    None => {
+                        warn!(
+                            "default handler: Received update without chat_id: {:?}",
+                            update
+                        )
+                    }
+                }
+                Ok(())
+            }),
+        );
+
+    info!("Starting Telegram bot...");
+
+    // registering command hints
+    match bot.set_my_commands(Command::bot_commands()).await {
+        Ok(_) => (),
+        Err(error) => {
+            error!("error while registering bot commands: {}", error);
+            ()
+        }
+    };
+
+    Dispatcher::builder(bot, main_handler)
+        .dependencies(dptree::deps![tip_context.unwrap()])
+        .error_handler(LoggingErrorHandler::with_custom_text(
+            "An error has occurred in the dispatcher",
+        ))
+        .enable_ctrlc_handler()
+        .build()
+        .dispatch()
+        .await;
+}
+
+async fn command_handler(
+    bot: Bot,
+    msg: Message,
+    tip_context: Arc<TipContext>,
+    cmd: Command,
+) -> Result<(), TelegramBotError> {
+    let cloned_message = msg.clone();
+    let from = match msg.from {
+        Some(from) => from,
+        None => {
+            bot.send_message(msg.chat.id, "You must be a user to use this bot.")
+                .await?;
+            return Ok(());
+        }
+    };
+
+    if from.is_bot {
+        bot.send_message(msg.chat.id, "You must be a user to use this bot.")
+            .await?;
+        return Ok(());
+    }
+
+    if from.username.is_none() {
+        bot.send_message(msg.chat.id, "You must have a username to use this bot.")
+            .await?;
+        return Ok(());
+    }
+
+    let tip_user = TipUser::from(from);
+
+    match cmd {
+        Command::Help => {
+            bot.send_message(cloned_message.chat.id, Command::descriptions().to_string())
+                .await?;
+        }
+        Command::NetworkId => {
+            let network_id = tip_context.network_id();
+            bot.send_message(
+                cloned_message.chat.id,
+                format!("Network ID: {}", network_id),
+            )
+            .await?;
+        }
+        Command::Create { password } => {
+            command_create(bot, &cloned_message, tip_context, &tip_user, password).await?
+        }
+        Command::Status => command_status(bot, &cloned_message, tip_context, &tip_user).await?,
+        Command::Open { password } => {
+            command_open(bot, &cloned_message, tip_context, &tip_user, password).await?
+        }
+        Command::Close => command_close(bot, &cloned_message, tip_context, &tip_user).await?,
+        Command::Destroy => command_destroy(bot, &cloned_message, tip_context, &tip_user).await?,
+        Command::Export { password } => {
+            command_export(bot, &cloned_message, tip_context, &tip_user, password).await?
+        }
+        Command::ChangePassword {
+            old_password,
+            new_password,
+        } => {
+            command_change_password(
+                bot,
+                &cloned_message,
+                tip_context,
+                &tip_user,
+                old_password,
+                new_password,
+            )
+            .await?
+        }
+        Command::Restore { mnemonic, password } => {
+            command_restore(
+                bot,
+                &cloned_message,
+                tip_context,
+                &tip_user,
+                mnemonic,
+                password,
+            )
+            .await?
+        }
+        Command::Send {
+            amount,
+            password,
+            telegram_username,
+            message, //optional tip message~
+        } => {
+            command_send(
+                bot,
+                &cloned_message,
+                tip_context,
+                &tip_user,
+                password,
+                amount,
+                telegram_username,
+                message, //used to pass the message to function
+            )
+            .await?
+        }
+        Command::Withdraw {
+            address,
+            amount,
+            password,
+        } => {
+            command_withdraw(
+                bot,
+                &cloned_message,
+                tip_context,
+                &tip_user,
+                password,
+                address,
+                amount,
+            )
+            .await?
+        }
+        Command::Claim => command_claim(bot, &cloned_message, tip_context, &tip_user).await?,
+    }
+    Ok(())
+}
